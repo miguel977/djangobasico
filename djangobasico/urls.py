a1126938f2df9e6b3d1df65615e05ee6b7f2d525
miguel977 <miguel645@gmail.com>
2025-01-21 10:58:50 -0300

    Aprendendo Mia sobre templates

--- djangobasico/urls.py
+++ djangobasico/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from inicio.views import index
 
 urlpatterns = [
+    path('', index),
     path('admin/', admin.site.urls),
     path('produtos/', include('produtos.urls')),
     path('clientes/', include('clientes.urls')),
