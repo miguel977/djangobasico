c536b0c239f59774d92e92bd9062c0f1971e3382
miguel977 <miguel645@gmail.com>
2025-01-22 07:46:43 -0300

    Aprendendo mais sobre templates

--- produtos/urls.py
+++ produtos/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.pagina_produtos ),
+    path('', views.index ),
     path('celulares/', views.celulares ),
 ]
--- produtos/views.py
+++ produtos/views.py
@@ -3,11 +3,33 @@ from django.shortcuts import render
 Tem os recursos para fazer a exibição 
 utilizando o html e css (templates)
 """
-from django.http import HttpResponse
 
 # Create your views here.
-def pagina_produtos(request):
-    return HttpResponse('Página de produtos')
+def index(request):
+    # processamento, banco de dados
+    idade = 15
+    tipo = ''
+
+    if idade < 12:
+        tipo = 'Criança'
+    elif idade >= 12 and idade <= 18:
+        tipo = 'Adolescente'
+    else:
+        tipo = 'Adulto'
+
+    context = {
+        'nome': 'Jamilton Damasceno',
+        'ultimo_acesso': '10/10/2030',
+        'idade': 10,
+        'tipo': tipo,
+        'produtos': [
+            {'nome': 'Notebook Acer', 'preco': '1.200,00'},
+            {'nome': 'Iphone', 'preco': '2.200,00'},
+            {'nome': 'Samsung', 'preco': '4.200,00'},
+        ]
+    }
+
+    return render(request, 'index.html', context)
 
 def celulares(request):
-    return HttpResponse('Página de celulares')
+    return render(request, 'celulares.html')
